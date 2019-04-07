e17424de8d0ff8e5caf40cbd4ff261b99b206526
Paresh Mahajan <pmahajan@equalexperts.com>
2019-04-07 20:11:16 +0530

    move the functionality belonging to the careerApply page to the careerApply page object class

--- src/test/java/pageobjects/CareerApplyPage.java
+++ src/test/java/pageobjects/CareerApplyPage.java
@@ -3,48 +3,177 @@ package pageobjects;
 import com.google.inject.Inject;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import support.World;
 
 public class CareerApplyPage extends BasePage {
 
-    @FindBy(tagName= "h2")
-    public WebElement h2Tag;
+    private static final String ERROR_MESSAGE_FOR_BLANK_FIELD = "Please fill out this field.";
+
+    @FindBy(tagName = "h2")
+    private WebElement h2Tag;
 
     @FindBy(css = "button.btn:nth-child(1)")
-    public WebElement submitBtn;
+    private WebElement submitBtn;
 
-    @FindBy(id="Title")
-    public WebElement titleText;
+    @FindBy(id = "Title")
+    private WebElement titleText;
 
-    @FindBy(id="Full Name")
-    public WebElement fullNameText;
+    @FindBy(id = "Full Name")
+    private WebElement fullNameText;
 
-    @FindBy(id="Email")
-    public WebElement emailText;
+    @FindBy(id = "Email")
+    private WebElement emailText;
 
-    @FindBy(id="Address")
-    public WebElement addressText;
+    @FindBy(id = "Address")
+    private WebElement addressText;
 
-    @FindBy(xpath="//input[@placeholder='Phone']")
-    public WebElement phoneText;
+    @FindBy(xpath = "//input[@placeholder='Phone']")
+    private WebElement phoneText;
 
-    @FindBy(xpath="//input[@type='checkbox']")
-    public WebElement termsAndConditionsChkBox;
+    @FindBy(xpath = "//input[@type='checkbox']")
+    private WebElement termsAndConditionsChkBox;
 
-    @FindBy(name="file")
-    public WebElement uploadFile;
+    @FindBy(name = "file")
+    private WebElement uploadFile;
 
-    @FindBy(name="position")
-    public WebElement positionSelectList;
+    @FindBy(name = "position")
+    private WebElement positionSelectList;
 
-    @FindBy(name="jobtype")
-    public WebElement jobTypeSelectList;
+    @FindBy(name = "jobtype")
+    private WebElement jobTypeSelectList;
 
-    @FindBy(id="career-form")
-    public WebElement careerForm;
+    @FindBy(id = "career-form")
+    private WebElement careerForm;
 
     @Inject
     public CareerApplyPage(World world) {
         super(world);
+    }
+
+    public void verifyCareerApplyPageRendered() {
+        getWebDriverWait().until(ExpectedConditions.textToBePresentInElement(h2Tag,
+                "APPLY NOW TO JOIN US"));
+    }
+
+    public void clickSubmitButton() {
+        getWebDriverWait().until(ExpectedConditions.elementToBeClickable(submitBtn)).click();
+    }
+
+    public void assertValidationMessageWhenFullNameIsMissing() {
+
+        getWebDriverWait().until(
+                ExpectedConditions.attributeContains(
+                        fullNameText,
+                        "validationMessage",
+                        ERROR_MESSAGE_FOR_BLANK_FIELD)
+        );
+    }
+
+    public void enterFullName(String fullName) {
+        getWebDriverWait().until(ExpectedConditions.visibilityOf(fullNameText));
+        fullNameText.sendKeys(fullName);
+    }
+
+    public void assertValidationMessageWhenEmailIsMissing() {
+        getWebDriverWait().until(
+                ExpectedConditions.attributeContains(
+                        emailText,
+                        "validationMessage",
+                        ERROR_MESSAGE_FOR_BLANK_FIELD)
+        );
+    }
+
+    public void enterEmail(String email) {
+        getWebDriverWait().until(ExpectedConditions.visibilityOf(emailText));
+        emailText.sendKeys(email);
+    }
+
+    public void assertValidationMessageWhenPhoneIsMissing() {
+        getWebDriverWait().until(
+                ExpectedConditions.attributeContains(
+                        phoneText,
+                        "validationMessage",
+                        ERROR_MESSAGE_FOR_BLANK_FIELD)
+        );
+    }
+
+    public void enterPhone(String phoneNumber) {
+        getWebDriverWait().until(ExpectedConditions.attributeContains(
+                phoneText, "id", "phone"));
+        phoneText.sendKeys(phoneNumber);
+    }
+
+    public void assertValidationMessageWhenResumeIsMissing() {
+        String errorMessage = "Please select a file.";
+        getWebDriverWait().until(
+                ExpectedConditions.attributeContains(
+                        uploadFile,
+                        "validationMessage",
+                        errorMessage)
+        );
+    }
+
+    public void selectFileToUpload() {
+        getWebDriverWait().until(ExpectedConditions.visibilityOf(uploadFile));
+        String filePath = "/Users/pareshmahajan/career/src/test/resources/data/resume.txt";
+        uploadFile.sendKeys(filePath);
+    }
+
+    public void assertValidationMessageWhenTermsAndConditionCheckBoxIsNotChecked() {
+        String errorMessage = "Please check this box if you want to proceed.";
+        getWebDriverWait().until(
+                ExpectedConditions.attributeContains(
+                        termsAndConditionsChkBox,
+                        "validationMessage",
+                        errorMessage)
+        );
+    }
+
+    public void assertValidationMessageWhenEmailIsInvalid() {
+        String errorMessage = "Please enter an email address.";
+        getWebDriverWait().until(
+                ExpectedConditions.attributeContains(
+                        emailText,
+                        "validationMessage",
+                        errorMessage)
+        );
+    }
+
+    public void enterTitle(String title) {
+        getWebDriverWait().until(ExpectedConditions.visibilityOf(fullNameText));
+        titleText.sendKeys(title);
+    }
+
+    public void enterAddress(String address) {
+        getWebDriverWait().until(ExpectedConditions.visibilityOf(addressText));
+        addressText.sendKeys(address);
+    }
+
+    public void selectPosition(String position) {
+        getWebDriverWait().until(ExpectedConditions.visibilityOf(positionSelectList));
+        Select positionDropDown = new Select(positionSelectList);
+        positionDropDown.selectByVisibleText(position);
+    }
+
+    public void selectJobType(String jobType) {
+        getWebDriverWait().until(ExpectedConditions.visibilityOf(jobTypeSelectList));
+        Select jobTypeDropDown = new Select(jobTypeSelectList);
+        jobTypeDropDown.selectByVisibleText(jobType);
+    }
+
+    public void clickTermsAndConditionsCheckBox() {
+        getWebDriverWait().until(ExpectedConditions.elementToBeClickable(termsAndConditionsChkBox)).click();
+    }
+
+    public void assertSuccessfulMessage() {
+        String successMessage = "<strong>Success</strong> Your message was sent successfully! We will be in touch as soon as we can.";
+        getWebDriverWait().until(
+                ExpectedConditions.attributeContains(
+                        careerForm,
+                        "innerHTML",
+                        successMessage)
+        );
     }
 }
--- src/test/java/steps/CareerApplyStepDef.java
+++ src/test/java/steps/CareerApplyStepDef.java
@@ -19,135 +19,91 @@ public class CareerApplyStepDef {
 
     @Then("^Career Apply Page is rendered$")
     public void careerApplyPageIsRendered() {
-        careerApplyPage.getWebDriverWait().until(ExpectedConditions.textToBePresentInElement(careerApplyPage.h2Tag,
-                "APPLY NOW TO JOIN US"));
+        careerApplyPage.verifyCareerApplyPageRendered();
     }
 
     @When("^User clicks the Submit Button$")
     public void userClicksTheSubmitButton() {
-        careerApplyPage.getWebDriverWait().until(ExpectedConditions.elementToBeClickable(careerApplyPage.submitBtn))
-                .click();
+        careerApplyPage.clickSubmitButton();
     }
 
     @Then("^User should see the validation message to fill the Full Name$")
     public void userShouldSeeTheValidationMessageToFillTheFullName()  {
-        careerApplyPage.getWebDriverWait().until(
-                ExpectedConditions.attributeContains(
-                        careerApplyPage.fullNameText,
-                        "validationMessage",
-                        "Please fill out this field."));
+        careerApplyPage.assertValidationMessageWhenFullNameIsMissing();
     }
 
     @When("^User sets the 'Full Name' as \"([^\"]*)\"$")
     public void userSetsTheFullNameAs(String fullName) {
-        careerApplyPage.getWebDriverWait().until(ExpectedConditions.visibilityOf(careerApplyPage.fullNameText));
-        careerApplyPage.fullNameText.sendKeys(fullName);
+        careerApplyPage.enterFullName(fullName);
     }
 
     @Then("^User should see the validation message to fill the Email$")
     public void userShouldSeeTheValidationMessageToFillTheEmail() {
-        careerApplyPage.getWebDriverWait().until(
-                ExpectedConditions.attributeContains(
-                        careerApplyPage.emailText,
-                        "validationMessage",
-                        "Please fill out this field."));
+        careerApplyPage.assertValidationMessageWhenEmailIsMissing();
     }
 
     @And("^User sets the 'Email' as \"([^\"]*)\"$")
     public void userSetsTheEmailAs(String email) {
-        careerApplyPage.getWebDriverWait().until(ExpectedConditions.visibilityOf(careerApplyPage.emailText));
-        careerApplyPage.emailText.sendKeys(email);
+        careerApplyPage.enterEmail(email);
     }
 
     @Then("^User should see the validation message to fill the Phone$")
     public void userShouldSeeTheValidationMessageToFillThePhone() {
-        careerApplyPage.getWebDriverWait().until(
-                ExpectedConditions.attributeContains(
-                        careerApplyPage.phoneText,
-                        "validationMessage",
-                        "Please fill out this field."));
+        careerApplyPage.assertValidationMessageWhenPhoneIsMissing();
     }
 
     @And("^User sets the 'Phone' as \"([^\"]*)\"$")
     public void userSetsThePhoneAs(String phoneNumber) {
-        careerApplyPage.getWebDriverWait().until(ExpectedConditions.attributeContains(
-                careerApplyPage.phoneText, "id", "phone"));
-        careerApplyPage.phoneText.sendKeys(phoneNumber);
+        careerApplyPage.enterPhone(phoneNumber);
     }
 
     @Then("^User should see the validation message to Upload a Resume file$")
     public void userShouldSeeTheValidationMessageToUploadAResumeFile() {
-        careerApplyPage.getWebDriverWait().until(
-                ExpectedConditions.attributeContains(
-                        careerApplyPage.uploadFile,
-                        "validationMessage",
-                        "Please select a file."));
+        careerApplyPage.assertValidationMessageWhenResumeIsMissing();
     }
 
     @And("^User selects the file to upload$")
     public void userSelectsTheFileToUpload() {
-        careerApplyPage.getWebDriverWait().until(ExpectedConditions.visibilityOf(careerApplyPage.uploadFile));
-        careerApplyPage.uploadFile.sendKeys("/Users/pareshmahajan/career/src/test/resources/data/resume.txt");
+        careerApplyPage.selectFileToUpload();
     }
 
     @Then("^User should see the validation message for the 'Terms and Conditions' checkbox$")
     public void userShouldSeeTheValidationMessageForTheTermsAndConditionsCheckbox() {
-        String errorMessage = "Please check this box if you want to proceed.";
-        careerApplyPage.getWebDriverWait().until(
-                ExpectedConditions.attributeContains(
-                        careerApplyPage.termsAndConditionsChkBox,
-                        "validationMessage",
-                        errorMessage));
+        careerApplyPage.assertValidationMessageWhenTermsAndConditionCheckBoxIsNotChecked();
     }
 
     @Then("^User should see the validation message to enter a valid email address$")
     public void userShouldSeeTheValidationMessageToEnterAValidEmailAddress() {
-        careerApplyPage.getWebDriverWait().until(
-                ExpectedConditions.attributeContains(
-                        careerApplyPage.emailText,
-                        "validationMessage",
-                        "Please enter an email address."));
+        careerApplyPage.assertValidationMessageWhenEmailIsInvalid();
     }
 
     @When("^User sets the 'Title' as \"([^\"]*)\"$")
     public void userSetsTheTitleAs(String title) {
-        careerApplyPage.getWebDriverWait().until(ExpectedConditions.visibilityOf(careerApplyPage.fullNameText));
-        careerApplyPage.titleText.sendKeys(title);
+        careerApplyPage.enterTitle(title);
     }
 
     @And("^User sets the 'Address' as \"([^\"]*)\"$")
     public void userSetsTheAddressAs(String address) {
-        careerApplyPage.getWebDriverWait().until(ExpectedConditions.visibilityOf(careerApplyPage.addressText));
-        careerApplyPage.addressText.sendKeys(address);
+        careerApplyPage.enterAddress(address);
     }
 
     @And("^User selects the 'Position' as \"([^\"]*)\"$")
     public void userSelectsThePositionAs(String position) {
-        careerApplyPage.getWebDriverWait().until(ExpectedConditions.visibilityOf(careerApplyPage.positionSelectList));
-        Select positionDropDown = new Select(careerApplyPage.positionSelectList);
-        positionDropDown.selectByVisibleText(position);
+        careerApplyPage.selectPosition(position);
     }
 
     @And("^User selects the 'Job Type' as \"([^\"]*)\"$")
     public void userSelectsTheJobTypeAs(String jobType) {
-        careerApplyPage.getWebDriverWait().until(ExpectedConditions.visibilityOf(careerApplyPage.jobTypeSelectList));
-        Select jobTypeDropDown = new Select(careerApplyPage.jobTypeSelectList);
-        jobTypeDropDown.selectByVisibleText(jobType);
+        careerApplyPage.selectJobType(jobType);
     }
 
     @And("^User clicks the 'Terms and Conditions' checkbox$")
     public void userClicksTheTermsAndConditionsCheckbox() {
-        careerApplyPage.getWebDriverWait().until(ExpectedConditions.elementToBeClickable(careerApplyPage.termsAndConditionsChkBox))
-                .click();
+        careerApplyPage.clickTermsAndConditionsCheckBox();
     }
 
     @Then("^User should see the details submitted successfully message$")
     public void userShouldSeeTheDetailsSubmittedSuccessfullyMessage()  {
-        String successMessage = "<strong>Success</strong> Your message was sent successfully! We will be in touch as soon as we can.";
-        careerApplyPage.getWebDriverWait().until(
-                ExpectedConditions.attributeContains(
-                        careerApplyPage.careerForm,
-                        "innerHTML",
-                        successMessage));
+        careerApplyPage.assertSuccessfulMessage();
     }
 }
