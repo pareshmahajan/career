5690df361207d4d93186039ab9030d3837741f74
Paresh Mahajan <pmahajan@equalexperts.com>
2019-04-07 23:37:24 +0530

    minor clean up

--- src/test/java/steps/CareerApplyStepDef.java
+++ src/test/java/steps/CareerApplyStepDef.java
@@ -1,7 +1,6 @@
 package steps;
 
 import com.google.inject.Inject;
-import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
